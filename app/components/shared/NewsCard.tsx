de66bb50d83a7d4a57b3a2d80251e566f07f35dc
Naofel-Badsha <naofelbadsha@gmail.com>
2026-01-16 11:27:54 +0600

    added default news card

---
+++ app/components/shared/NewsCard.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const NewsCard = () => {
+    return (
+        <section className="border p-4 rounded-md shadow-md">
+            <Link href="#">
+                <Image
+                    src="/assets/banner.jpg"
+                    alt="Banner images"
+                    width={560}
+                    height={420}
+                    className='mb-5 md:h-56 rounded-md hover:scale-105 cursor-pointer transition-all duration-200'
+                />
+
+            </Link>
+            <div className="">
+                <h2 className='text-2xl font-semibold my-3'>What To Expect From United Natural Foods Inc (UNFI) Q4 2024 Earni</h2>
+                <p className='mb-4 text-[18px]'>United Natural Foods Inc (UNFI) will release its Q4 2024 earnings on October 1, 2024</p>
+                <Link href="#">
+                    <Button variant="default">Read More</Button>
+                </Link>
+            </div>
+        </section>
+    )
+}
+
+export default NewsCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Banner from "./components/shared/Banner";
+import NewsCard from "./components/shared/NewsCard";
 
 export default function Home() {
   return (
     <div className="text-5xl py-24">
        <Banner />
+
+       <div className="my-12">
+        <h1 className="text-2xl font-bold mb-8">Latest News</h1>
+        <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+          <NewsCard />
+        </div>
+       </div>
       </div>
   );
 }
