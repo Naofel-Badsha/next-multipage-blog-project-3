97ae7fd3a6abe5650313c4a8420816c68dcb8a8b
Naofel-Badsha <naofelbadsha@gmail.com>
2026-01-17 12:35:39 +0600

    added fecth api intregation

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,9 +1,10 @@
 
 
-const fetchNews = async (category: string = "", search: ="") =>{
+const fetchNews = async (category: string = "", search: string="") =>{
     try{
-
-const response = await fetch(``)
+const response = await fetch(`https://news-api-next-js-kappa.vercel.app/api/news?
+category=${category}&search=${search}`);
+// const response = await fetch(`https://naofel-badsha.github.io/api/db.json`)
     }catch(error){
         console.error("Error fetching news data", error);
         return []
